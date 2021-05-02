52d83a920cbecfb4610c62d7f7e290b6ab1cc1d8
Eduardo Steiner Hessel <eduardo.hessel@fatec.sp.gov.br>
2021-05-02 19:00:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,246 @@
+<html lang="pt-br">
+    <head>
+
+        <?php
+        include_once 'head.php';
+        ?>
+
+    </head>
+
+    <body id="page-top">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <?php include_once 'nav.php'; ?>
+        </nav>
+
+        <header class="masthead text-center text-white d-flex">
+            <div class="container my-auto">
+                <div class="row">
+                    <div class="col-lg-10 mx-auto">
+                        <h1 class="h1-title text-uppercase">
+                            <strong>SUA CASA DE UMA FORMA NUNCA ANTES VISTA!</strong>
+                        </h1>
+                        <hr>
+                    </div>
+                    <div class="col-lg-8 mx-auto">
+                        <p class="text-faded mb-5" style="color: white; text-shadow: black 0.1em 0.1em 0.2em;   ">Confira todas as vantagens de uma casa automatizada!</p>
+                        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#sobre">Saiba Mais</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <section class="bg-primary" id="sobre">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto text-center">
+                        <h2 class="section-heading text-white">Já imaginou a facilidade que uma casa inteligente te proporciona?</h2>
+                        <hr class="light my-4">
+                        <p class="text-faded mb-4">
+                            Uma casa automatizada facilita sua vida das mais diferentes maneiras:
+                            seja na rapidez para te trazer agilidade no seu cotidiano,
+                            na hora de proteger sua casa, na hora de lazer e conforto
+                            e da praticidade que ela oferece, devido a possibilidade de controlar,
+                            tanto dentro como fora dela, luzes, televisões, câmeras de segurança,
+                            aparelhos condicionadores de ar e até mesmo a porta e o portão.
+                            Venha conhecer alguns dos vários benefícios que uma casa inteligente pode te trazer!
+                        </p>
+                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#beneficios">Benefícios</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="beneficios">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading">Benefícios</h2>
+                        <hr class="my-4">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box mt-5 mx-auto">
+                            <i class="fa fa-4x fa-lock text-primary mb-3 sr-icons"></i>
+                            <h3 class="mb-3">Mais segurança!</h3>
+                            <p class="text-muted mb-0">Abra sua porta com um cartão e uma senha e seu portão com seu smartphone!</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box mt-5 mx-auto">
+                            <i class="fa fa-4x fa-camera text-primary mb-3 sr-icons"></i>
+                            <h3 class="mb-3">Mais controle!</h3>
+                            <p class="text-muted mb-0">Controle as câmeras da casa à distância via smartphone!</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box mt-5 mx-auto">
+                            <i class="fa fa-4x fa-lightbulb-o text-primary mb-3 sr-icons"></i>
+                            <h3 class="mb-3">Mais facilidade!</h3>
+                            <p class="text-muted mb-0">Controle as lâmpadas de sua casa sem precisar se levantar do sofá!</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box mt-5 mx-auto">
+                            <i class="fa fa-4x fa-television text-primary mb-3 sr-icons"></i>
+                            <h3 class="mb-3">Mais conforto!</h3>
+                            <p class="text-muted mb-0">Jogue seus controles no lixo! Controle sua TV e seu ar-condicionado pelo seu celular!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="contato" class="bg-dark text-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto text-center">
+                        <h2 class="section-heading">Entre em contato conosco!</h2>
+                        <hr class="my-4">
+                        <p class="mb-5">Gostou da ideia e dos benefícios? Então entre em contato conosco!</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 ml-auto text-center">
+                        <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
+                        <p>(11) 96318-4361</p>
+                    </div>
+                    <div class="col-lg-4 mr-auto text-center">
+                        <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
+                        <p>
+                            contato.willdomus@gmail.com
+                        </p>
+                    </div>
+                    <div class="col-lg-8 mx-auto text-center">
+                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#projetos">Imagens do Projeto</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="p-0" id="projetos">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters popup-gallery">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="imagem/imgsite1.jpg">
+                            <img class="img-fluid" src="imagem/imgsite1.jpg" alt="">
+                            <div class="portfolio-box-caption">
+                                <div class="portfolio-box-caption-content">
+                                    <div class="project-category text-faded">
+                                        Maquete
+                                    </div>
+                                    <div class="project-name">
+                                        Topográfica
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="imagem/imgsite2.jpg">
+                            <img class="img-fluid" src="imagem/imgsite2.jpg" alt="">
+                            <div class="portfolio-box-caption">
+                                <div class="portfolio-box-caption-content">
+                                    <div class="project-category text-faded">
+                                        Maquete
+                                    </div>
+                                    <div class="project-name">
+                                        Quarto e Cozinha
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="imagem/imgsite3.jpg">
+                            <img class="img-fluid" src="imagem/imgsite3.jpg" alt="">
+                            <div class="portfolio-box-caption">
+                                <div class="portfolio-box-caption-content">
+                                    <div class="project-category text-faded">
+                                        Maquete
+                                    </div>
+                                    <div class="project-name">
+                                        Ligações
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="imagem/imgsite4.jpg">
+                            <img class="img-fluid" src="imagem/imgsite4.jpg" alt="">
+                            <div class="portfolio-box-caption">
+                                <div class="portfolio-box-caption-content">
+                                    <div class="project-category text-faded">
+                                        Maquete
+                                    </div>
+                                    <div class="project-name">
+                                        Ligações
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="imagem/imgsite5.jpg">
+                            <img class="img-fluid" src="imagem/imgsite5.jpg" alt="">
+                            <div class="portfolio-box-caption">
+                                <div class="portfolio-box-caption-content">
+                                    <div class="project-category text-faded">
+                                        Placas
+                                    </div>
+                                    <div class="project-name">
+                                        Arduino
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="imagem/imgsite6.jpg">
+                            <img class="img-fluid" src="imagem/imgsite6.jpg" alt="">
+                            <div class="portfolio-box-caption">
+                                <div class="portfolio-box-caption-content">
+                                    <div class="project-category text-faded">
+                                        Placas
+                                    </div>
+                                    <div class="project-name">
+                                        Raspberry Pi 3
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <section id="entrar">
+            <div class="container text-center">
+                <h2 class="mb-4">Já é cliente? Faça Login!</h2>
+                <hr>
+                <a class="btn btn-primary btn-xl js-scroll-trigger" href="admin/">Entrar</a>
+                <br>
+                <br>
+                <a style="color:black; font-weight: bold" href="admin/primeiroAcesso/">Primeiro acesso.</a>
+            </div>
+        </section>
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+
+        <!-- Plugin JavaScript -->
+        <script src="js/jquery.easing.min.js"></script>
+        <script src="js/scrollreveal.min.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+
+        <!-- Custom scripts for this template -->
+        <script src="js/creative.min.js"></script> 
+    </body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">Will Domus</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#sobre">Sobre</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#beneficios">Benefícios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#contato">Contato</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#projetos">Projeto</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#entrar">Entrar</a>
+            </li>
+        </ul>
+    </div>
+</div>
